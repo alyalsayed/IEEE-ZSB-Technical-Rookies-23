0f2b580e9f11d1f81edaaa7bc28ca19e666760a6
Abdoo <20812019100880@eng.zu.edu.eg>
2022-11-11 00:51:00 +0200

    I solved Task-1 in cpp

---
+++ Task-1/main.cpp
@@ -0,0 +1,14 @@
+/* A program to calculate the area and perimeter
+ * of a rectangle given the length and the width 
+ * created by Aly Alsayed
+ * */
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int length,width;// two variable to store 
+    cin>>length>>width;// get the input 
+    cout<<(length*width)<<endl;//output the area
+    cout<<(length+width)*2 ;//output the perimeter
+}
