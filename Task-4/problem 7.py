4396136494866cc0f27a027da3827105240de8f6
Abdoo <20812019100880@eng.zu.edu.eg>
2022-12-02 19:47:29 +0200

    task done

--- Task-4/problem 7.py
+++ Task-4/problem 7.py
@@ -1,19 +1,24 @@
 def get_anagrams(list1):
-       dic = {}
-       for word in list1:
-           new_word = ''.join(sorted(word))
-           if new_word not in dic:
-               dic[new_word] = [word]
-           else:
-               dic[new_word] += [word]
-       return [dic[i] for i in dic]
+    dic = {}
+    for word in list1:
+        new_word = ''.join(sorted(word))
+        if new_word not in dic:
+            dic[new_word] = [word]
+        else:
+            dic[new_word] += [word]
+    return [dic[i] for i in dic]
+
 
 if __name__ == '__main__':
-   n=int(input())
-   list1=[]
-   for _ in range(n):
-       list1.append(input())
-   print(get_anagrams(list1))
+    n = int(input())
+    list1 = []
+    for _ in range(n):
+        list1.append(input())
+    res=get_anagrams(list1)
+    for i in res:
+        for j in i:
+            print(j,end=" ")
+        print()
 '''
 example:
 list>>["eat" ,"tea" ,"tan", "ate", "nat" ,"bat"]
